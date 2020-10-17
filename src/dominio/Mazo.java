442ca5b9a2702c3f6bf83d428ce313ae66461690
Matías Maximiliano Simone <matias.maximiliano.simone@gmail.com>
2020-10-17 00:33:48 -0300

    Mesa firmas

--- src/dominio/Mazo.java
+++ src/dominio/Mazo.java
@@ -1,5 +1,7 @@
 package dominio;
 
-public class Mazo extends Carta {
+import java.util.TreeSet;
+
+public class Mazo {
 
 }
--- src/dominio/Mesa.java
+++ src/dominio/Mesa.java
@@ -1,5 +1,51 @@
 package dominio;
 
-public class Mesa extends Carta {
+import java.util.TreeSet;
+
+public class Mesa {
+	Mazo mazo;
+	TreeSet<Jugador> jugadores;
+	Jugador turno;
+	Carta cartaApartada;
+	int cantSimbolosDeAfectoNecesario;
 	
+	public Mesa() {
+		
+	}
+	
+	public void repartir() {
+		
+	}
+	
+	public void agregarJugador(Jugador jugador) {
+		
+	}
+	
+	public void intercambiarJugador(Jugador jugador) {
+		
+	}
+	
+	public Carta darCarta(Jugador jugador) {
+		return null;
+	}
+	
+	public boolean nombrarCarta(Jugador jugador, Carta carta) {
+		return false;
+	}
+	
+	public boolean verCartas(Jugador jugador, Carta carta) {
+		return false;
+	}
+	
+	public boolean mostrarseCartas(Jugador jugador) {
+		return false;
+	}
+	
+	public void descartarTomarCarta(Jugador jugador) {
+		
+	}
+	
+	public Carta jugarInmediatamente() {
+		return null;
+	}
 }
