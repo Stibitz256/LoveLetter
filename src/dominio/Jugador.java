53cef6c6e480a7d03c8e14f5ec350c3a8354d2e5
omalvasso <malvassooctavio@gmail.com>
2020-10-17 00:10:25 -0300

    modificacion extends en jugador

--- src/dominio/Jugador.java
+++ src/dominio/Jugador.java
@@ -1,5 +1,58 @@
 package dominio;
 
-public class Jugador extends Carta {
-	
+
+public class Jugador {
+	String nombre;
+	boolean protegido;
+	boolean eliminado;
+	boolean rendido;
+	int simbolosAfectos;
+	LinkedHashSet cartasDescartadas<Carta>;
+	LinkedHashSet cartasMano<Carta>;
+
+	public Jugador(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Carta descartar(Carta) {
+		
+	}
+
+//	public Carta tomarCarta(Carta) {
+//		
+//	}
+
+	public void eliminar() {
+
+	}
+
+	public void proteger() {
+
+	}
+
+	public void desproteger() {
+
+	}
+
+	public void rendirse() {
+
+	}
+
+	public boolean estaEliminado() {
+
+	}
+
+	public boolean estaRendido() {
+
+	}
+
+	public int obtenerSimbolosAfectos() {
+
+	}
+
+	public LinkedHashSet<Carta> obtenerCartasDescartadas() {
+
+	}
+
+
 }
