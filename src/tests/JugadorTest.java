aeacac09368862cebafd9d56dc9d170f7e114c67
cristian <delvallecristian@gmail.com>
2020-10-17 00:38:10 -0300

    Creacion de las clases de Test

---
+++ src/tests/JugadorTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JugadorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
