bc7f9e02348cba192614cbd05b9ef51d08fad7aa
Matías Maximiliano Simone <matias.maximiliano.simone@gmail.com>
2020-11-07 21:06:28 -0300

    test finalizar ronda

--- src/tests/RondaTest.java
+++ src/tests/RondaTest.java
@@ -1,5 +1,7 @@
 package tests;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Comparator;
 import java.util.TreeSet;
 
@@ -7,26 +9,21 @@ import org.junit.Test;
 
 import dominio.entidad.Jugador;
 import dominio.entidad.Ronda;
+import junit.framework.Assert;
 
 public class RondaTest {
 
 	@Test
 	public void testFinalizarRonda() {
-		
-		TreeSet<Jugador> jugadores= new TreeSet<Jugador>(new Comparator<Object>() {
-			@Override
-			public int compare(Object o1, Object o2) {
-				// TODO Auto-generated method stub
-				return 1;
-			}
-		});
-		jugadores.add(new Jugador("pablo"));
+		TreeSet<Jugador> jugadores = new TreeSet<Jugador>();
+		Jugador jugador = new Jugador("pablo");
+		jugadores.add(jugador);
 		jugadores.add(new Jugador("kevin"));
-		Ronda juego= new Ronda(jugadores);
-		
+		Ronda juego = new Ronda(jugadores);
+
 		Jugador ganador = juego.finalizarRonda();
-		
-		System.out.println(ganador.toString());
+
+		assertEquals(jugador, ganador);
 	}
 
 }
